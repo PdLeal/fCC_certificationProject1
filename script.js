eee2973e2f66f3f8122a62abe39a2e0b182de706
PdLeal <143546507+PdLeal@users.noreply.github.com>
2023-08-30 10:45:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("submit");
+
+function submit() {
+    button.textContent = "I'm useless, but thanks for clicking me =)"
+}
+button.addEventListener("click", submit);
